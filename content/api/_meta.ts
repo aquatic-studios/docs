c43ef87a7ba04ab0e89be665f438ce448b8dfa06
senkex <ssenkex@gmail.com>
2026-08-30 11:36:37 -0300

    feat(content): add AquaLib, HeadRender and AquaAPI references

---
+++ content/api/_meta.ts
@@ -0,0 +1,5 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+  aquaapi: 'AquaAPI'
+} satisfies MetaRecord
---
+++ content/libraries/_meta.ts
@@ -0,0 +1,6 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+  aqualib: 'AquaLib',
+  headrender: 'HeadRender'
+} satisfies MetaRecord
