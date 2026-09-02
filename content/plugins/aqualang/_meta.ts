10e4ef4481d6f54974f11441cd420e0128bfd2e1
senkex <ssenkex@gmail.com>
2026-09-01 23:25:54 -0300

    docs(aquaspawn, aqualang): document join events, spawns, sync and language API

---
+++ content/plugins/aqualang/_meta.ts
@@ -0,0 +1,8 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+  commands: 'Commands',
+  configuration: 'Configuration',
+  github: 'GitHub Sync',
+  api: 'Developer API'
+} satisfies MetaRecord
---
+++ content/plugins/aquaspawn/_meta.ts
@@ -0,0 +1,7 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+  commands: 'Commands',
+  spawns: 'Spawns',
+  'join-events': 'Join Events'
+} satisfies MetaRecord
