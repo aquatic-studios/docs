fbd16775e601a4750ef428f39f2e262e374b1aec
senkex <ssenkex@gmail.com>
2026-09-01 23:12:14 -0300

    docs(aqualive, aquatac): document platforms, broadcasts, board and betting rules

---
+++ content/plugins/aqualive/_meta.ts
@@ -0,0 +1,8 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+  commands: 'Commands',
+  platforms: 'Platforms',
+  broadcast: 'Broadcast',
+  webhook: 'Discord Webhook'
+} satisfies MetaRecord
---
+++ content/plugins/aquatac/_meta.ts
@@ -0,0 +1,7 @@
+import type { MetaRecord } from 'nextra'
+
+export default {
+  commands: 'Commands',
+  configuration: 'Configuration',
+  menu: 'Board'
+} satisfies MetaRecord
